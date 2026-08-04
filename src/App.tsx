2ade9dca22fa1fe1c7e18f40040da5f9df75d894
Breno <brenofranzin@gmail.com>
2026-08-04 16:08:10 -0300

    corrige promocao automatica da lista de espera ao cancelar matricula; ativa notificacao de turmas em 10min

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import AppRoutes from "./routes";
-
+import { useNotificacaoAulas } from "./hooks/useNotificacaoAulas";
+import { useTurmas } from "@/modules/turmas/hooks/useTurmas";
+import { useModalidades } from "@/modules/modalidades/hooks/useModalidades";
 
 export default function App(){
+  const { turmas } = useTurmas();
+  const { modalidades } = useModalidades();
 
-return(
+  useNotificacaoAulas(turmas, modalidades);
 
-<AppRoutes/>
-
-)
-
+  return(
+    <AppRoutes/>
+  )
 }
