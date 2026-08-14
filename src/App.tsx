f0d839a38e4b63a8cde236cc59ad36bf64e08cfc
Breno <brenofranzin@gmail.com>
2026-08-14 15:36:43 -0300

    Inicia syncQueue no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useEffect } from "react";
 import AppRoutes from "./routes";
 import { useNotificacaoAulas } from "./hooks/useNotificacaoAulas";
 import { useTurmas } from "@/modules/turmas/hooks/useTurmas";
 import { useModalidades } from "@/modules/modalidades/hooks/useModalidades";
-
+import { syncQueueService } from "@/lib/syncQueue.service";
 export default function App(){
   const { turmas } = useTurmas();
   const { modalidades } = useModalidades();
-
   useNotificacaoAulas(turmas, modalidades);
-
+  useEffect(() => {
+    syncQueueService.iniciar();
+  }, []);
   return(
     <AppRoutes/>
   )
